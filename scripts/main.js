a92b01778e2303816062ede56113b58cfbd8b1d8
Omar Martinez <this.is.omartinez@gmail.com>
2014-06-07 01:58:59 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+var Portfolio = Portfolio || {};
+
+Portfolio = {
+  init: function() {
+    if($('.landing').length) {
+      this.landing();
+    }
+
+    if($('.project-page').length) {
+      this.project();
+    }
+  },
+  landing: function() {
+    // hover effect
+    $('ul.showcase li').hover(
+      function() {
+        $( this ).addClass( "hovered" );
+      }, function() {
+        $( this ).removeClass( "hovered" );
+      });
+  },
+  project: function() {
+    $('.swipebox').swipebox({
+      hideBarsDelay: 5000
+    });
+  }
+}
+
+$(function(){
+  Portfolio.init();
+});
+
+$(window).load(function(){
+    $('body').removeClass('preload');
+});
